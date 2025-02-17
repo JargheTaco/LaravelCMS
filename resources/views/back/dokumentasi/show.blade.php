a64e28870d6bdae3ca6b0f9be80179b66b722111
Jarghe Taco <jarghetaco@email.com>
2025-02-17 11:48:07 +0700

    Update project without LFM images

--- resources/views/back/dokumentasi/show.blade.php
+++ resources/views/back/dokumentasi/show.blade.php
@@ -41,8 +41,8 @@
             <tr>
                 <th>Image</th>
                 <td>
-                    <a href="{{ asset('storage/app/public/back/'.$dokumentasi->img) }}" target="_blank" rel="noopener noreferrer">
-                    <img src="{{ asset('storage/app/public/back/'.$dokumentasi->img) }}" alt="" width="50%">
+                    <a href="{{ asset('storage/back/'.$dokumentasi->img) }}" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ asset('storage/back/'.$dokumentasi->img) }}" alt="" width="50%">
                     </a>
                 </td>
             </tr>
