9b84c30400434dd5741f9222e3ed510b4c5e0ffd
Jarghe Taco <jarghetaco@email.com>
2025-02-17 10:18:01 +0700

    Fix storage issue and update deployment

--- resources/views/back/article/show.blade.php
+++ resources/views/back/article/show.blade.php
@@ -54,7 +54,7 @@
                 <th>Image</th>
                 <td>
                     <a href="{{ asset('storage/back/'.$article->img) }}" target="_blank" rel="noopener noreferrer">
-                    <img src="{{ asset('storage/back/'.$article->img) }}" alt="" width="50%">
+                    <img src="{{ asset('storage/back/'.$article->img) }}" alt="Gambar" width="50%">
                     </a>
                 </td>
             </tr>
