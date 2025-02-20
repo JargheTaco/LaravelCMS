c933ff6cf3ef0a0d077ee42304cd6668ab4755da
Jarghe Taco <jarghetaco@email.com>
2025-02-20 09:50:59 +0700

    Berita github

--- resources/views/front/dashboard/galeri.blade.php
+++ resources/views/front/dashboard/galeri.blade.php
@@ -1,5 +1,5 @@
-@include ('front.layout.navbar')
-@include ('front.layout.assets')
+@include('front.layout.assets')
+@include('front.layout.navbar')
 
 <section class="page-section bg-light" id="portfolio">
     <div class="container">
@@ -74,15 +74,8 @@
 @endforeach
 
 
-<!-- Bootstrap core JS-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-<!-- Core theme JS-->
-<script src="https://laravel-project-self.vercel.app/back/js/navbar.js"></script>  
-<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-<!-- * *                               SB Forms JS                               * *-->
-<!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
-<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+@include('front.layout.footer')
+@include('front.layout.scripts')
 </body>
 
 </html>
