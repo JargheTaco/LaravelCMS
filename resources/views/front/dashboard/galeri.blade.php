30f8109f9bd25cab4c4b22d0a9ff35aed00ec6fb
Jarghe Taco <jarghetaco@email.com>
2025-02-20 09:40:38 +0700

    Berita github

--- resources/views/front/dashboard/galeri.blade.php
+++ resources/views/front/dashboard/galeri.blade.php
@@ -48,35 +48,31 @@
     </div>
 </footer>
 <!-- Portfolio item 1 modal popup-->
-<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-        @foreach ($dokumentasi as $item)
-            <div class="close-modal" data-bs-dismiss="modal"><img
-                    src="{{ asset('front/img/close-icon.svg') }}" alt="Close modal" /></div>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8">
-                        <div class="modal-body">
-                        
-                            <!-- Project details-->
-                            <h2 class="text-uppercase">{{ $item->title }}</h2>
-                            <img class="img-fluid d-block mx-auto"
-                                src="{{ $item->img }}" alt="..." />
-                            <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal"
-                                type="button">
-                                <i class="fas fa-xmark me-1"></i>
-                                Close Project
-                            </button>
-                        
+@foreach ($dokumentasi as $item)
+    <div class="portfolio-modal modal fade" id="portfolioModal{{ $item->id }}" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-bs-dismiss="modal">
+                    <img src="{{ asset('front/img/close-icon.svg') }}" alt="Close modal" />
+                </div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <div class="modal-body">
+                                <h2 class="text-uppercase">{{ $item->title }}</h2>
+                                <img class="img-fluid d-block mx-auto" src="{{ $item->img }}" alt="..." />
+                                <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                    <i class="fas fa-xmark me-1"></i> Close Project
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        @endforeach
         </div>
     </div>
-</div>
+@endforeach
+
 
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
