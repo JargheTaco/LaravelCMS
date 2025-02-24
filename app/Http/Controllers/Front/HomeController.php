b42108dce15d3c33baa51eacce9aaeb54b89609a
Jarghe Taco <jarghetaco@email.com>
2025-02-24 20:30:51 +0700

    tugas dan fungsi

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -58,8 +58,8 @@ class HomeController extends Controller
 
         return view("front.dashboard.profil.profil" . $profileNumber , [
             'profilpejabat' => ProfilPejabat::whereStatus(1)->paginate(6),
-            'tugasfungsi' => Tugasfungsi::whereStatus(1),
-            'struktur' => Tugasfungsi::whereStatus(1),
+            'tugasfungsi' => Tugasfungsi::whereStatus(1)->get(),
+            'struktur' => Tugasfungsi::whereStatus(1)->get(),
         ]);
     }
 
