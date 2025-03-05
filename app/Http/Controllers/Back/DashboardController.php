f2d06ea86fd78551f14d6289d5fe70a14e595304
Jarghe Taco <jarghetaco@email.com>
2025-03-05 21:25:49 +0700

    informasi

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Article;
 use App\Models\Berita;
 use App\Models\Category;
+use App\Models\Informasiberkala;
+use App\Models\Pengumuman;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -16,7 +18,11 @@ class DashboardController extends Controller
             'total_articles'    => Article::count(),
             'total_beritas'  => Berita::count(),
             'latest_article'    => Article::with('Category')->whereStatus(1)->latest()->take(5)->get(),
-            'latest_berita'   => Berita::with('Category')->whereStatus(1)->latest()->take(5)->get()
+            'latest_berita'   => Berita::with('Category')->whereStatus(1)->latest()->take(5)->get(),
+            'latest_informasiberkala'   => Informasiberkala::with('Category')->whereStatus(1)->latest()->take(5)->get(),
+            'latest_pengumuman'   => Pengumuman::with('Category')->whereStatus(1)->latest()->take(5)->get(),
+            'total_informasiberkala'   => Pengumuman::count(),
+            'total_pengumuman'   => Informasiberkala::count(),
         ]);
     }
 }
--- resources/views/back/dashboard/index.blade.php
+++ resources/views/back/dashboard/index.blade.php
@@ -32,6 +32,30 @@
       </div>
     </div>
 
+    <div class="col-6">
+      <div class="card text-bg-success mb-3" style="max-width: 100%;">
+        <div class="card-header">Total Pengumuman</div>
+        <div class="card-body">
+          <h5 class="card-title">{{ $total_pengumuman }} Berita</h5>
+          <p class="card-text">
+            <a href="{{ url('pengumuman')}}" class="text-white">View Categories</a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-6">
+      <div class="card text-bg-success mb-3" style="max-width: 100%;">
+        <div class="card-header">Informasi Berkala</div>
+        <div class="card-body">
+          <h5 class="card-title">{{ $total_informasiberkala }} Berita</h5>
+          <p class="card-text">
+            <a href="{{ url('informasiberkala')}}" class="text-white">View Categories</a>
+          </p>
+        </div>
+      </div>
+    </div>
+
     <div class="row">
       <div class="col-6">
         <h4>Latest Articles</h4>
@@ -94,27 +118,25 @@
     </div>
     <div class="row">
       <div class="col-6">
-        <h4>Latest Articles</h4>
+        <h4>Latest Pengumuman</h4>
         <table class="table table-bordered table-striped">
           <thead>
             <tr>
               <th>No</th>
               <th>Title</th>
-              <th>Category</th>
               <th>Create At</th>
               <th>Action</th>
             </tr>
           </thead>
 
           <tbody>
-            @foreach($latest_article as $item)
+            @foreach($latest_pengumuman as $item)
             <tr>
               <td>{{ $loop->iteration }}</td>
               <td>{{ $item->title }}</td>
-              <td>{{ $item->Category->name }}</td>
               <td>{{ $item->created_at }}</td>
               <td class="text-center">
-                <a href="{{ url('article/' .$item->id) }}" class="btn btn-sm btn-secondary">Detail</a>
+                <a href="{{ url('pengumuman/' .$item->id) }}" class="btn btn-sm btn-secondary">Detail</a>
               </td>
             </tr>
             @endforeach
@@ -123,27 +145,25 @@
       </div>
 
       <div class="col-6">
-        <h4>Latest Berita</h4>
+        <h4>Latest Informasi Berkala</h4>
         <table class="table table-bordered table-striped">
           <thead>
             <tr>
               <th>No</th>
               <th>Title</th>
-              <th>Category</th>
               <th>Create At</th>
               <th>Action</th>
             </tr>
           </thead>
 
           <tbody>
-            @foreach($latest_berita as $item)
+            @foreach($latest_informasiberkala as $item)
             <tr>
               <td>{{ $loop->iteration }}</td>
               <td>{{ $item->title }}</td>
-              <td>{{ $item->Category->name }}</td>
               <td>{{ $item->created_at }}</td>
               <td class="text-center">
-                <a href="{{ url('berita/' .$item->id) }}" class="btn btn-sm btn-secondary">Detail</a>
+                <a href="{{ url('informasiberkala/' .$item->id) }}" class="btn btn-sm btn-secondary">Detail</a>
               </td>
             </tr>
             @endforeach
