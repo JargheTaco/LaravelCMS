96060dd980acc6bd494e2995f729cef0272ec888
Jarghe Taco <jarghetaco@email.com>
2025-03-05 11:23:02 +0700

    renja

--- resources/views/front/layout/headrenja.blade.php
+++ resources/views/front/layout/headrenja.blade.php
@@ -2,27 +2,4 @@
 <header class="header2" style="padding-top: 100px">
     <h1>PROGRAM & KEGIATAN</h1>
     <h2>RENJA</h2>
-    <h3> @switch($title)
-            @case('renja 2020-2023')
-                renja 2020-2023
-            @break
-
-            @default
-                renja 2024-2026
-        @endswitch
-    </h3>
-    <div class="divider2"></div>
 </header>
-
-<div class="container2" style="padding-bottom:0px">
-    <section class="document-section2">
-        <h3> @switch($title)
-            @case('renja 2020-2023')
-                renja 2020-2023
-            @break
-
-            @default
-                renja 2024-2026
-        @endswitch</h3>
-    </section>
-</div>
