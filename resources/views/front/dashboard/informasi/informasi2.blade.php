6865bf7203e9449d6f7d8aa3669c016ffe2951be
Jarghe Taco <jarghetaco@email.com>
2025-03-05 16:03:32 +0700

    informasi

--- resources/views/front/dashboard/informasi/informasi2.blade.php
+++ resources/views/front/dashboard/informasi/informasi2.blade.php
@@ -1,59 +1,23 @@
 @include('front.layout.assets')
 @include('front.layout.navbar')
 @include('front.layout.headinfor',['title' => 'Laporan Informasi Publik'])
-    <div class="container mt-5">
-        <div class="row mt-3">
-            <div class="col-md-3">
-                <select class="form-select" id="tahunSelect">
-                    <option value="">Tahun</option>
-                    <option value="2023">2023</option>
-                    <option value="2022">2022</option>
-                    <option value="2021">2021</option>
-                </select>
-            </div>
-            <div class="col-md-6">
-                <input type="text" class="form-control" id="searchInput" placeholder="Cari berdasarkan judul...">
-            </div>
+<div class="container2" style="padding-top: 0px">
+    @foreach($laporaninformasi as $item)
+    <section class="document-section2">
+
+        <div class="document-item2">
+            <h4>{{$item->title}}</h4>
+            <tr>
+                <td colspan="2">
+                    <div class="iframe-container text-center">
+                        <iframe src="https://docs.google.com/gview?url={{ urlencode($item->pdf) }}&embedded=true" width="100%" height="500" allow="autoplay"></iframe>
+                    </div>
+                </td>
+            </tr>
         </div>
-        <table class="table mt-4">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Judul</th>
-                    <th>File</th>
-                </tr>
-            </thead>
-            <tbody id="tableBody">
-                <tr data-tahun="2023" data-judul="Aset dan Inventarisasi Barang Tahun 2023">
-                    <td>1</td>
-                    <td>Aset dan Inventarisasi Barang Tahun 2023</td>
-                    <td class="button-group3">
-                        <a href="#" class="btn-custom btn-custom-download-1"><i class="fa fa-download"></i> Download</a>
-                        <a href="#" class="btn-custom btn-custom-read-2"><i class="fa fa-book"></i> Baca</a>
-                        <a href="#" class="btn-custom btn-custom-link-3"><i class="fa fa-link"></i> Link</a>
-                    </td>
-                </tr>
-                <tr data-tahun="2022" data-judul="Aset dan Inventarisasi Barang Tahun 2022">
-                    <td>2</td>
-                    <td>Aset dan Inventarisasi Barang Tahun 2022</td>
-                    <td class="button-group3">
-                        <a href="#" class="btn-custom btn-custom-download-1"><i class="fa fa-download"></i> Download</a>
-                        <a href="#" class="btn-custom btn-custom-read-2"><i class="fa fa-book"></i> Baca</a>
-                        <a href="#" class="btn-custom btn-custom-link-3"><i class="fa fa-link"></i> Link</a>
-                    </td>
-                </tr>
-                <tr data-tahun="2021" data-judul="Aset dan Inventarisasi Barang Tahun 2021">
-                    <td>3</td>
-                    <td>Aset dan Inventarisasi Barang Tahun 2021</td>
-                    <td class="button-group3">
-                        <a href="#" class="btn-custom btn-custom-download-1"><i class="fa fa-download"></i> Download</a>
-                        <a href="#" class="btn-custom btn-custom-read-2"><i class="fa fa-book"></i> Baca</a>
-                        <a href="#" class="btn-custom btn-custom-link-3"><i class="fa fa-link"></i> Link</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    </section>
+    @endforeach
+</div>
 
 @include('front.layout.footer')
 @include('front.layout.scripts')
